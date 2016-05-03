46aca4c5cc68ec6b5ecc0323c306bd0b0e9cfeb8
Neil Cook <neil.james.cook@gmail.com>
2016-05-03 09:02:39 +0100

    Update neil_math_functions.py

--- neil_math_functions.py
+++ neil_math_functions.py
@@ -48,3 +48,32 @@ def interp1d(x, y):
     sclass = spline_return(tck)
     return sclass
 
+
+def rchisquared(x, y, ey, model, p):
+    """
+    Calculates the reduced chisquared value based on x and y and a model
+    :param x: [numpy array] x axis data (data to base model on)
+    :param y: [numpy array] y axis data (data to fit model to)
+    :param ey: [numpy array] uncertainties in y axis data
+    :param model: [function] function taking arguements x and *p
+                             i.e. lambda x, *p: p[0]*x + p[1]*x
+    :param p: [list] list of parameters for model for a model with 2 fit
+                     parameters p = [a, b] and function would require
+                     x, a, b as arguments
+    :return: reduced chi squared, degrees of freedom (N - n - 1)
+    """
+    chi2 = np.sum(pow(((y - model(p, x)) / ey), 2))
+    DOF = len(y) - len(p) - 1
+    rchi2 = chi2 / float(DOF)
+    return rchi2, DOF
+
+
+def chisquare(y1, y2, ey1):
+    """
+    calculate chi squared for a data set (with uncertainties) and a model
+    :param y1: array data
+    :param y2: array model
+    :param ey1: array uncertainties on data
+    :return:
+    """
+    return np.sum(((y1 - y2) / ey1) ** 2)
